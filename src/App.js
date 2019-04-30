d026e70684072db6cae2f506303d252bf7617765
Student <example@example.com>
2019-04-29 19:22:50 -0700

    switch back to ternary expressions

--- src/App.js
+++ src/App.js
@@ -1,96 +1,42 @@
 import React, { Component } from "react";
 import "./App.css";
 import ImageSlider from "./components/ImageSlider";
-import Counter from "./components/Counter";
-import Header from "./components/Header";
-
+import Counter from "./components/Counter"
 class App extends Component {
+  // add(a, b) {
+  //   return a + b;
+  // }
+
   state = {
-    visible: true,
-    whichComponentToShow: "ImageSlider"
+    visible: true
   };
 
-  render() {
-    if (this.state.whichComponentToShow === "ImageSlider") {
-      return (
-        <div className="App">
-          <ImageSlider />
-          <button
-            onClick={() => {
-              this.setState({ whichComponentToShow: "Counter" });
-            }}
-          >
-            show counter
-          </button>
-        </div>
-      );
-    } else if (this.state.whichComponentToShow === "Counter") {
-      return (
-        <div className="App">
-          <Counter />
-          <button
-            onClick={() => {
-              this.setState({ whichComponentToShow: "Header" });
-            }}
-          >
-            show header
-          </button>
-        </div>
-      );
-    } else if (this.state.whichComponentToShow === "Header") {
-      return (
-        <div className="App">
-          <Header />
-        </div>
-      );
-    }
 
-    return null;
+  render() {
+
+    const buttonText = this.state.visible ? 'hide' : 'show';
+    const slider = this.state.visible ? (
+      <ImageSlider />
+    ) : (
+      <div> <Counter /></div>
+    );
+
+    return (
+      <div className="App">
+        {slider}
+        <button
+          onClick={() => {
+            this.setState({
+              visible: !this.state.visible
+            });
+
+          }}
+        >
+        {buttonText}
+        </button>
+      </div>
+    );
   }
 }
 
 export default App;
-
-
-// import React, { Component } from "react";
-// import "./App.css";
-// import ImageSlider from "./components/ImageSlider";
-// import Counter from "./components/Counter"
-// class App extends Component {
-//   // add(a, b) {
-//   //   return a + b;
-//   // }
-//
-//   state = {
-//     visible: true
-//   };
-//
-//
-//   render() {
-//
-//     const buttonText = this.state.visible ? 'hide' : 'show';
-//     const slider = this.state.visible ? (
-//       <ImageSlider />
-//     ) : (
-//       <div> <Counter /></div>
-//     );
-//
-//     return (
-//       <div className="App">
-//         {slider}
-//         <button
-//           onClick={() => {
-//             this.setState({
-//               visible: !this.state.visible
-//             });
-//
-//           }}
-//         >
-//         {buttonText}
-//         </button>
-//       </div>
-//     );
-//   }
-// }
-//
-// export default App;
